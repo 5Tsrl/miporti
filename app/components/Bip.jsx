84af399fa182e7e054bf6615e3b0c25365c22a6f
roby2107 <roby2107@users.noreply.github.com>
2016-05-31 17:30:17 +0200

    Link colli.jsx in new tab

--- app/components/Bip.jsx
+++ app/components/Bip.jsx
@@ -6,8 +6,8 @@ export default () =>(
     <div className="widget widget_ticket">
         <h2 className="title-2">Biglietto integrato Piemonte</h2>
         <p>Scopri di pi&ugrave; sul sistema regionale di bigliettazione elettronica.</p>
-        <a href="http://www.bip.piemonte.it"><span  className="img_ticket" alt="biglietto bip" ></span></a>
-        <a href="http://www.bip.piemonte.it" className="btn_link">Vai al sito</a>
+        <a target="_blank" href="http://www.bip.piemonte.it"><span  className="img_ticket" alt="biglietto bip" ></span></a>
+        <a target="_blank" href="http://www.bip.piemonte.it" className="btn_link">Vai al sito</a>
     </div>
   
 )
