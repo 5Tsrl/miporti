b524246e9b1a10083320988e8f78ac29be686215
roby2107 <roby2107@users.noreply.github.com>
2016-05-31 17:04:22 +0200

    Aggiornato testo Cookie.jsx

--- app/pages/Cookie.jsx
+++ app/pages/Cookie.jsx
@@ -6,6 +6,68 @@ export default () =>
         <h2 className="pageHeader" >Cookie</h2>
         <div className="pageContent">
 
-             <p>Cookie.</p>
+            <h2>UTILIZZO DEI COOKIE</h2>
+            <p>
+            Ai sensi del provvedimento n. 229 del 8 maggio 2014 <strong>“Individuazione delle modalità semplificate per l'informativa e l'acquisizione del consenso per l'uso dei cookie”</strong>
+            del Garante per la protezione dei dati personali, si informa che il sito <strong><a href="http://www.muoversinpiemonte.it/">www.muoversinpiemonte.it</a></strong> utilizza cookies 
+            al fine di rendere la navigazione del sito sicura ed efficiente e che
+            non consentono l'acquisizione di dati personali identificativi dell'utente. Utilizzando il sito web <strong><a href="http://www.muoversinpiemonte.it/">www.muoversinpiemonte.it</a></strong>, 
+            l'utente dichiara di accettare e acconsentire all’ utilizzo dei cookies in conformità con i termini di uso dei cookies espressi in questa informativa.
+            </p>
+            <h2>Che cosa sono i cookie?</h2>
+            <p>
+            I cookie sono file di testo che contengono pacchetti di informazioni che vengono memorizzati sul tuo computer o sul tuo dispositivo mobile tutte le volte che visiti un sito online
+            attraverso un browser. Ad ogni successiva visita il browser invia questi cookies al sito web che li ha originati o ad un altro sito. I cookies permettono ai siti di ricordare
+            alcune informazioni per permetterti di navigare online in modo semplice e veloce.
+            </p>
+            <h2>I cookie presenti sul sito Muoversi in Piemonte</h2>
+            <p>
+            Questo sito internet utilizza solo cookies tecnici ed analitici, che possono essere utilizzati senza chiedere il consenso dell’utente, poiché sono strettamente necessari per
+            l’esplorazione del sito e la fruizione dei servizi presenti.
+            Qui di seguito sono riportati i tipi cookie che possono essere utilizzati nel Sito con una descrizione della finalità legata all'uso.
+            </p>
+            <h2>Cookie di sessione o di navigazione</h2>
+            <p>
+            I cookie di questo tipo sono necessari per il corretto funzionamento di alcune aree del sito.  I cookie di questa categoria comprendono sia cookie persistenti che cookie di
+            sessione. In assenza di tali cookie, il sito o alcune porzioni di esso potrebbero non funzionare correttamente. Pertanto, vengono sempre utilizzati, indipendentemente dalle
+            preferenze dall'utente. I cookie di questa categoria vengono sempre inviati dal dominio di Muoversi in Piemonte.
+            </p>
+            <h2>Cookie tecnici</h2>
+            <p>
+            I cookie di questo tipo vengono utilizzati per raccogliere informazioni sull'utilizzo del sito. Il Titolare usa tali informazioni per analisi statistiche, per migliorare
+            il sito e semplificarne l'utilizzo, oltre che per monitorarne il corretto funzionamento. Questo tipo di cookie raccoglie informazioni in forma anonima sull'attività degli
+            utenti nel sito e sul modo in cui sono arrivati al Sito e alle pagine visitate. I cookie di questa categoria vengono inviati dal Sito stesso o da domini di terze parti.
+            A tal proposito, si ricorda che su questo sito è in corso una rilevazione statistica tramite l'utilizzo dello strumento Google Analytics, che utilizza modalità di analisi
+            secondo illustrate dal documento relativo alle <a href="http://www.google.it/policies/privacy/">Norme generiche sulla privacy</a> di Google e dal documento specifico in
+            merito all'utilizzo delle <a href="http://www.google.com/analytics/terms/it.html">informazioni raccolte con Google Analytics</a>.
+            </p>
+            <h2>Come disabilitare i cookie</h2>
+            <p>
+            Nel caso si volesse bloccare cookie, si ricorda che questo potrebbe avere un impatto negativo sull’usabilità del sito web. La maggior parte dei browser permette
+             di rifiutare/accettare i cookie. Di seguito si riportano alcune informazioni pratiche per disabilitare i cookie sul browser che si sta utilizzando.
+            </p>
+            <h2>Google Chrome</h2>
+            <p>
+            Cliccare la chiave inglese nell’angolo in alto a destra e selezionare 'Impostazioni'. A questo punto selezionare 'Mostra impostazioni avanzate’ (“Under the hood'”)
+            e cambiare le impostazioni della 'Privacy'.
+            </p>
+            <h2>Microsoft Internet Explorer</h2>
+            <p>
+            Cliccare l’icona 'Strumenti' nell’angolo in alto a destra e selezionare 'Opzioni internet'. Nella finestra pop up selezionare 'Privacy'. Qui potrete regolare le
+            impostazioni dei vostri cookies.
+            </p>
+            <h2>Mozilla Firefox</h2>
+            <p>
+            Dal menu a tendina nell’angolo in alto a sinistra selezionare 'Opzioni'. Nella finestra di pop up selezionare 'Privacy'. Qui potrete regolare le impostazioni dei
+            vostri cookies.
+            </p>
+            <h2>Safari</h2>
+            <p>
+            Dal menu di impostazione a tendina nell’angolo in alto a destra selezionare 'Preferenze'. Selezionare 'Sicurezza' e qui potrete regolare le impostazioni dei
+            vostri cookies.</p><p>
+            Per disabilitare i cookie analitici e impedire a Google Analytics di raccogliere dati sulla propria navigazione, si può scaricare il componente aggiuntivo del browser
+            per la disattivazione di Google Analytics: <a href="https://tools.google.com/dlpage/gaoptout">https://tools.google.com/dlpage/gaoptout</a>.
+            </p>
+            
         </div>
     </div>
