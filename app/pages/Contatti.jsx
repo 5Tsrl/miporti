d8f9bc737ed548c305dfbfd1c4369755b444ec79
roby2107 <roby2107@users.noreply.github.com>
2016-05-31 16:55:51 +0200

    Aggiornato testo Contatti.jsx

--- app/pages/Contatti.jsx
+++ app/pages/Contatti.jsx
@@ -3,11 +3,14 @@ import './page.scss';
 
 export default () =>
     <div className="widget page">
-        <h2 className="pageHeader" >Contatti</h2>
+        <h2 className="pageHeader">Contatti</h2>
         <div className="pageContent">
 
-             <p>Per segnalazioni e suggerimenti, scrivi a <a href="mailto:muoversinpiemonte@5t.torino.it">muoversinpiemonte@5t.torino.it</a> o contatta il Numero Verde Unico
-                della Regione Piemonte 800333444 attivo dal lunedì al venerdì dalle ore 8 alle 18 con orario continuato,
-                festivi esclusi, gratuito da telefono fisso e mobile.</p>
+            <p>Per segnalazioni e suggerimenti:</p><br />
+                <ul  className="liv2">
+                    <li>scrivi a <strong><a href="mailto:muoversinpiemonte@5t.torino.it">muoversinpiemonte@5t.torino.it</a></strong></li><br />
+                    <li>contatta il Numero Verde Unico della Regione Piemonte <strong>800 333 444</strong> attivo dal lunedì al venerdì 
+                    dalle ore 8 alle 18 con orario continuato, festivi esclusi, gratuito da telefono fisso e mobile.</li>
+                </ul>
         </div>
     </div>
