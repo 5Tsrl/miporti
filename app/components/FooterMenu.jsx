2da213f5eb23366824940709030339413b6063c2
roby2107 <crcastelli@tiscali.it>
2016-06-28 14:05:56 +0200

    Gestione FooterMenu inglese

--- app/components/FooterMenu.jsx
+++ app/components/FooterMenu.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect} from 'react-redux'
+import {update} from 'react-intl-redux'
 import { Link } from 'react-router'
 import axios from 'axios'
 import lscache from 'lscache'
@@ -11,50 +13,44 @@ const FooterMenuItem = React.createClass({
   }
 })
 
-const FooterMenu = React.createClass({
-    
-getInitialState: function () {
-    console.log(this.constructor.displayName)
-    // Check our localstorage cache, we may as well load from there if we have it
-    return lscache.get(this.constructor.displayName) || {}
+let FooterMenu = React.createClass({
+
+    getInitialState: function () {
+      // Check our localstorage cache, we may as well load from there if we have it
+      return  { footerMenu: lscache.get(`footerMenu_${this.props.currentLocale}`)
+              }
+  },
+
+
+  loadMenu: function() {
+          let menuId = 3 //it
+          if(this.props.currentLocale == 'en') menuId=6
+
+          axios
+              .get('http://mip.5t.torino.it/wp-json/wp-api-menus/v2/menus/'+menuId)
+              .then( (res) =>{
+                  //console.log('scaricato', `footerMenu_${this.props.currentLocale}`)
+                  this.setState({footerMenu: res.data})
+                  lscache.set(`footerMenu_${this.props.currentLocale}`,res.data,5)
+              })
+  },
+
+componentDidMount: function() {
+    if(this.state.footerMenu == null) {
+        this.loadMenu()
+    }
 },
 
-/**
- * Sets the localstorage state, and continues on to set the state of the React component
- */
-setLocalState: function (data) {
-    // Store in LocalStorage
-    lscache.set(this.constructor.displayName, data, 10);
-    // Store in Component State
-    this.setState(data);
-},
-  
-componentDidMount: function() {
-    if(typeof this.state.footerMenu === 'undefined') {
-        // Request our data again
-        axios
-            .get('http://mip.5t.torino.it/wp-json/wp-api-menus/v2/menus/3')
-            .then( (res) =>{
-                //console.log(res)
-                this.setLocalState({
-                    footerMenu: res.data
-                })
-            })
-    }
-    
-},
-    
 render: function() {
         if ( ! this.state.footerMenu ) {
-            //var footerMenuNodes = <li><Link to={this.props.url}>{this.props.title}</Link></li>
-            var footerMenuNodes = <FooterMenuItem title="home" url="/home"  />
-        /*   return (
+           var footerMenuNodes = <FooterMenuItem title="home" url="/home"  />
+           return (
                <div className="loading-wrap">
                    <div className="loading"><span className="fa fa-heart"></span> LOADING</div>
                </div>
-           )*/
+           )
        }else{
-        
+
             var footerMenuNodes = this.state.footerMenu.items.map( function(item, idx){
                 const baseurl="http://wpmip.5t.torino.it"
                 const url = '/home/page'+item.url.slice(baseurl.length,-1)
@@ -63,7 +59,7 @@ render: function() {
                 )
             }.bind(this))
         }
-        return (            
+        return (
             <nav className="footer-menu">
                 <ul>
                     {footerMenuNodes}
@@ -73,4 +69,10 @@ render: function() {
     }
 })
 
-module.exports = FooterMenu;
+//mappo l' intl.locale dello state di redux sulla props currentLocale
+function mapStateToProps(state) {
+  return { currentLocale: state.intl.locale }
+}
+
+FooterMenu = connect(mapStateToProps)(FooterMenu)
+export default FooterMenu
