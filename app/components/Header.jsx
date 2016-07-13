3769f8ad700712c2ed002121e935e06114643a39
aguiraf <aguiraf@gmail.com>
2016-07-13 16:28:35 +0200

    Fix default export - eliminato _className

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Menu from './Menu';
 
-export default () =>(
+const Header = () =>(
 
     <header className="clearfix header" id="branding">
         <div className="aux">
             <a href="#" id="logo_site-title">Muoversi in Piemonte</a>
             <Menu />
-            <a href="http://www.regione.piemonte.it/" id="logo_regPie" target="_blank" _className="logo-regione">Regione Piemonte</a>
+            <a href="http://www.regione.piemonte.it/" id="logo_regPie" target="_blank">Regione Piemonte</a>
         </div>
     </header>
 
 )
+export default Header
