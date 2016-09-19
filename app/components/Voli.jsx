6b6b4a92e49af2864ec172b919dff4a445f8997b
aguiraf <aguiraf@gmail.com>
2016-09-19 18:17:12 +0200

    Fix links aeroporti secondo currentLocale

--- app/components/Voli.jsx
+++ app/components/Voli.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from 'react'
+import { connect} from 'react-redux'
 import {FormattedMessage} from 'react-intl'
-import classNames from 'classnames';
+import classNames from 'classnames'
 import axios from 'axios'
 
-const Volo = React.createClass({
+let Volo = React.createClass({
   render: function() {
     return (
         <tr>
@@ -66,21 +67,40 @@ var VoliTabs = React.createClass({
 })
 
 var VoliFooter = React.createClass({
+  
   render: function() {
+    
+    const links = {
+      en: {
+        auto:'http://www.aeroportoditorino.it/en/tomove/parking-transport/by-car',
+        bus: 'http://www.aeroportoditorino.it/en/tomove/parking-transport/by-bus',
+        train: 'http://www.aeroportoditorino.it/en/tomove/parking-transport/by-train',
+        timetable:'http://www.aeroportoditorino.it/en/tofly/flights/timetable'
+      },
+      it:{
+        auto:'http://www.aeroportoditorino.it/it/tomove/trasporti-e-parcheggi/in-auto',
+        bus: 'http://www.aeroportoditorino.it/it/tomove/trasporti-e-parcheggi/in-bus',
+        train: 'http://www.aeroportoditorino.it/it/tomove/trasporti-e-parcheggi/in-treno',
+        timetable:'http://www.aeroportoditorino.it/it/tofly/voli/orario-generale'
+      }    
+    }
+    
     return (
         <div className="last_info">
             <p><FormattedMessage id='orari di volo aggiornati alle '/>{this.props.aggiornamento}</p>
             <span className="raggiungere">
                 <FormattedMessage id="Raggiungere l'aeroporto in "/>
-                <a href="http://www.aeroportoditorino.it/en/tomove/parking-transport/by-car" className="btn_partenze" target="_blank"><FormattedMessage id='auto'/></a>
-                    <a href="http://www.aeroportoditorino.it/en/tomove/parking-transport/by-bus" className="btn_partenze" target="_blank"><FormattedMessage id='bus'/></a>
-                        <a href="http://www.aeroportoditorino.it/en/tomove/parking-transport/by-train" className="btn_partenze" target="_blank"><FormattedMessage id='treno'/></a>
+                <a href={`${links[this.props.currentLocale].auto}`}  className="btn_partenze" target="_blank"><FormattedMessage id='auto'/></a>
+                    <a href={`${links[this.props.currentLocale].bus}`}  className="btn_partenze" target="_blank"><FormattedMessage id='bus'/></a>
+                        <a href={`${links[this.props.currentLocale].train}`}  className="btn_partenze" target="_blank"><FormattedMessage id='treno'/></a>
                 {/*
                 <a href="http://www.aeroportoditorino.it/it/tofly/voli/partenze-arrivi?orario=oggi" className="btn_partenze">Partenze di oggi</a>
                 <a href="http://www.aeroportoditorino.it/it/tofly/voli/partenze-arrivi?orario=domani&set=partenze" className="btn_partenze">Partenze di domani</a>
+                 href={`links.${this.props.currentLocale}.timetable`} 
+                
                 */}
             </span>
-            <a href="http://www.aeroportoditorino.it/it/tofly/voli/orario-generale"  className="btn_link" target="_blank"><FormattedMessage id='Orario Generale'/></a>
+            <a href={`${links[this.props.currentLocale].timetable}`} className="btn_link" target="_blank"><FormattedMessage id='Orario Generale'/></a>
         </div>
     )
   }
@@ -89,7 +109,7 @@ var VoliFooter = React.createClass({
 
 
 
-const Voli = React.createClass({
+let Voli = React.createClass({
     getInitialState: function() {
       return { partenzeArrivi: 'partenze', data: {}};
     },
@@ -133,15 +153,20 @@ const Voli = React.createClass({
         <div className="title-4">
             <h2><FormattedMessage id='orario voli'/></h2>
             <h3><FormattedMessage id='Aeroporto di Torino'/></h3>
-            <p><FormattedMessage id='In collaborazione con '/> <a href="http://www.aeroportoditorino.it/it/sagat" target="_blank">Sagat S.p.a.</a></p>
+            <p><FormattedMessage id='In collaborazione con '/> <a href={'http://www.aeroportoditorino.it/'+this.props.currentLocale+'/sagat'} target="_blank">Sagat S.p.a.</a></p>
         </div>
         <VoliTabs partenzeArrivi={this.state.partenzeArrivi} onCurrentChoosen={this.handleSceltaCurrent}/>
         <VoliTable partenzeArrivi={this.state.partenzeArrivi} voli={voli} />
-        <VoliFooter aggiornamento={aggiornamento} />
+        <VoliFooter aggiornamento={aggiornamento} currentLocale={this.props.currentLocale}/>
 
       </div>
     );
   }
 });
+//mappo l' intl.locale dello state di redux sulla props currentLocale
+function mapStateToProps(state) {
+  return { currentLocale: state.intl.locale }
+}
 
+Voli = connect(mapStateToProps)(Voli)
 export default Voli
