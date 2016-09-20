bffbb1dd79755acd39bc4282a2f6498a81f050ba
aguiraf <aguiraf@gmail.com>
2016-09-20 11:20:32 +0200

    Update viaggiaPiemonte

--- app/components/ViaggiaPiemonte.jsx
+++ app/components/ViaggiaPiemonte.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import {FormattedMessage} from 'react-intl'
+import React from 'react'
+import { Link } from 'react-router'
 
 export default () =>(
 
     <div className="widget_viaggia">
-        <h2 className="title-2"><FormattedMessage id='Viaggia Piemonte'/></h2>
-        <p>Traffico ferroviario piemontese in tempo reale</p>
-        <a target="_blank" href="http://www.regione.piemonte.it/trasporti/viaggiaPiemonte/index.htm"><span  className="img_viaggia" alt="biglietto bip" ></span></a>
-        <a target="_blank" href="http://www.regione.piemonte.it/trasporti/viaggiaPiemonte/index.htm" className="btn_link">Vai al sito</a>
+        <h2 className="title-2">Viaggia Piemonte</h2>
+        <p>Tutti i treni circolanti nel territorio regionale</p>
+        <Link to='/page/viaggiapiemonte'><span  className="img_viaggia" alt="biglietto bip" ></span></Link>
+        <Link to='/page/viaggiapiemonte' className="btn_link">Maggiori informazioni</Link>
     </div>
 
 )
