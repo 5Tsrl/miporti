1663e3f9ce91c97c7a88d548b05d3d23456dcdff
aguiraf <aguiraf@gmail.com>
2017-06-21 23:48:49 +0200

    Fix Twitter Timeline component

--- app/components/Tweet.jsx
+++ app/components/Tweet.jsx
@@ -3,19 +3,22 @@ import {FormattedMessage} from 'react-intl'
 import {Timeline} from 'react-twitter-widgets'
 
 class Tweet extends React.Component {
-  
+
   render() {
     return <div className="widget_twitter">
       <h2 className="title-2"><FormattedMessage id='Tweet di mobilità'/></h2>
-      <div id="_scroll_twit"> 
+      <div id="_scroll_twit">
         <Timeline
+          dataSource={{
+            sourceType: 'list',
+            ownerScreenName: '5Tlive',
+            slug: 'muoversinpiemonte'
+          }}
           widgetId={'723176655192133634'}
           options={{
-            username: '5TLive',
             height: '424',
             chrome: 'nofooter  noheader noborders noscrollbar',
           }}
-          
         />
       </div>
   </div>
